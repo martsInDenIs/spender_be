ca9b5815f74ed8269fde8a562392d40d75521d24
Denys Martsyn <aqezy21@gmail.com>
2024-08-09 15:11:47 +0300

    feat: add orm and db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { ConsoleLogger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RequestsModule } from './requests/requests.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), RequestsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.getOrThrow('DATABASE_HOST'),
+        port: +config.getOrThrow('DATABASE_PORT'),
+        username: config.getOrThrow('DATABASE_USERNAME'),
+        password: config.getOrThrow('DATABASE_PASSWORD'),
+        database: config.getOrThrow('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+    RequestsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, ConsoleLogger],
 })
