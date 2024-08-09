e1c23ace34d73d590223a10a58cccf0ecc948f1e
Denys Martsyn <aqezy21@gmail.com>
2024-08-09 15:10:58 +0300

    opts: using config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { ConsoleLogger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RequestsModule } from './requests/requests.module';
 
 @Module({
-  imports: [RequestsModule],
+  imports: [ConfigModule.forRoot(), RequestsModule],
   controllers: [AppController],
   providers: [AppService, ConsoleLogger],
 })
