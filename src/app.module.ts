9192dd0c7c56f629879b9a2e37e115724e3c3630
Денис Марцин <user@Deniss-Laptop.local>
2024-08-09 13:38:20 +0300

    feat: add a log about app's listening port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { ConsoleLogger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RequestsModule } from './requests/requests.module';
 
 @Module({
-  imports: [],
+  imports: [RequestsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConsoleLogger],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ConsoleLogger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000).then(() => {
+    app.get(ConsoleLogger).log('App is listening on port 3000!');
+  });
 }
 bootstrap();
