b2e0844e58accc6d2a668dd22c03e489fd911afd
Denys Martsyn <aqezy21@gmail.com>
2024-08-16 12:22:42 +0300

    refactor: remove unnecessary module

--- src/request_transaction/request_transaction.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RequestTransactionService } from './request_transaction.service';
-
-@Module({
-  providers: [RequestTransactionService],
-  exports: [RequestTransactionService],
-})
-export class RequestTransactionModule {}
